cbe66406037a353ff808876d57456e8816ba4cf3
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2023-02-05 11:52:49 -0400

    initial commit

---
+++ vue-with-composition-api-and-typescript/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { reactive, ref } from "@vue/reactivity";
+
+const state = reactive({
+  name: "Link" as string,
+  age: 25 as number | number
+});
+
+const end = ref<object>({
+  bairro: "Monte das Oliveiras" as string,
+  numero: 122 as number | string
+});
+</script>
+
+<template>
+  <div>
+    <p>{{ state.name }}</p>
+    <p>{{ end.bairro }}</p>
+  </div>
+</template>
+
+<style scoped></style>
